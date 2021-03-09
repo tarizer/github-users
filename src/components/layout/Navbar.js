f30aa378284e6ccd7cb67d071ccb059b736700e8
Tarik <49348194+tarizer@users.noreply.github.com>
2021-03-09 23:26:37 +0100

    Refactor stateless classes to functional components

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,26 +1,23 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 
-class Navbar extends Component {
-  static defaultProps = {
-    title: "Github User Finder",
-    icon: "fab fa-github",
-  };
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-    icon: PropTypes.string.isRequired,
-  };
+const Navbar = ({ title, icon }) => {
+  return (
+    <nav className="navbar bg-primary">
+      <h1>
+        <i className={icon} /> {title}
+      </h1>
+    </nav>
+  );
+};
 
-  render() {
-    const { title, icon } = this.props;
-    return (
-      <nav className="navbar bg-primary">
-        <h1>
-          <i className={icon} /> {title}
-        </h1>
-      </nav>
-    );
-  }
-}
+Navbar.defaultProps = {
+  title: "Github User Finder",
+  icon: "fab fa-github",
+};
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired,
+};
 
 export default Navbar;
